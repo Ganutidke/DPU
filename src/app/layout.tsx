9fb2b797713144d7024f1e126e419beba8c73b86
Ganutidke <ganeshtidake519@gmail.com>
2025-07-20 06:35:16 +0530

    chore: update dependencies and integrate Vercel Analytics
    - Added @vercel/analytics to package.json for analytics tracking.
    - Added cloudinary and mongoose dependencies to package.json.
    - Integrated Vercel Analytics component in layout.tsx to enable analytics on the main layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Dr. D. Y. Patil Arts, Commerce & Science College',
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Header />
         <main className="flex-grow">
           {children}
+          <Analytics />
         </main>
         <Footer />
         <Toaster />
